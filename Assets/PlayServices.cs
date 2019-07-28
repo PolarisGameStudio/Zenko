952bbb087be271de9f6e7ca8a93a709e10d7fc50
BernardoLv <contact@bernardolimon.com>
2019-07-28 04:51:36 -0500

    WORKING
    GPGS FIRST TEST WORKING

--- Assets/PlayServices.cs
+++ Assets/PlayServices.cs
@@ -1,34 +1,38 @@
-﻿// using System.Collections;
-// using GooglePlayGames;
-// using GooglePlayGames.BasicApi;
-// using UnityEngine;
+﻿using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
 
-// public class PlayServices : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         PlayGamesClientConfiguration.Builder builder = new PlayGamesClientConfiguration.Builder();
-//         PlayGamesPlatform.InitializeInstance(builder.Build());
-//         PlayGamesPlatform.DebugLogEnabled = true;
-//         PlayGamesPlatform.Activate();
+public class PlayServices : MonoBehaviour
+{
+    public GameObject teller;
+    public GameObject teller2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGamesClientConfiguration.Builder builder = new PlayGamesClientConfiguration.Builder();
+        PlayGamesPlatform.InitializeInstance(builder.Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
 
-//         Social.localUser.Authenticate((bool success, string err) => {
-//         	if(success){
-//         		Debug.Log("Login success");
-//         	}
-//         	else
-//         	{
-//         		Debug.Log("Login failed");
-//         		Debug.Log("Error : " + err);
+        Social.localUser.Authenticate((bool success, string err) => {
+        	if(success){
+        		Debug.Log("Login success");
+                teller.SetActive(true);
+        	}
+        	else
+        	{
+                teller2.SetActive(true);
+        		Debug.Log("Login failed");
+        		Debug.Log("Error : " + err);
 
-//         	}
-//         });
-//     }
+        	}
+        });
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
+    // Update is called once per frame
+    void Update()
+    {
         
-//     }
-// }
+    }
+}
